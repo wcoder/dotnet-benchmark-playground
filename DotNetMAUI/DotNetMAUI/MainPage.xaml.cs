137aa44c883edbebaa6825a723242ac5044a6a46
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-10-28 23:49:14 +0300

    WIP: Add MAUI support

--- DotNetMAUI/DotNetMAUI/MainPage.xaml.cs
+++ DotNetMAUI/DotNetMAUI/MainPage.xaml.cs
@@ -1,25 +1,68 @@
-﻿namespace DotNetMAUI;
+﻿using BenchmarkDotNet.Analysers;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using DotNetMAUI.Runners;
+
+namespace DotNetMAUI;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Start();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private async Task Start()
+    {
+        SetIsRunning(true);
+        try
+        {
+            var logger = new AccumulationLogger();
+            await Task.Run(() =>
+            {
+                var config = default(IConfig);
+#if DEBUG
+                config = new DebugInProcessConfig();
+#endif
+                var summary = BenchmarkRunner.Run<EmptyBenchmarks>(config);
+                MarkdownExporter.Console.ExportToLog(summary, logger);
+                ConclusionHelper.Print(logger,
+                        summary.BenchmarksCases
+                               .SelectMany(benchmark => benchmark.Config.GetCompositeAnalyser().Analyse(summary))
+                               .Distinct()
+                               .ToList());
+            });
+            SetSummary(logger.GetLog());
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine(exc);
+            await DisplayAlert("Error", exc.Message, "Ok");
+        }
+        finally
+        {
+            SetIsRunning(false);
+        }
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private void SetIsRunning(bool isRunning)
+    {
+        Indicator.IsRunning = isRunning;
+        Run.IsVisible = Summary.IsVisible = !isRunning;
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void SetSummary(string text)
+    {
+        Summary.Text = text;
+        var size = Summary.Measure(double.MaxValue, double.MaxValue).Request;
+        Summary.WidthRequest = size.Width;
+        Summary.HeightRequest = size.Height;
+    }
 }
-
-
---
+++ DotNetMAUI/DotNetMAUI/Runners/EmptyBenchmarks.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace DotNetMAUI.Runners
+{
+    public class EmptyBenchmarks
+    {
+        [Benchmark]
+        public void Test1()
+        {
+        }
+
+        [Benchmark(Description = "Test2 - description")]
+        public void Test2()
+        {
+        }
+    }
+}
+
